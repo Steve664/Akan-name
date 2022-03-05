b02072b19d49f7e8561c908b431501248eb8b02a
steve kariuki <steve.kariuki@student.moringaschool.com>
2022-03-05 13:49:38 +0300

    Added javascript function and variables

---
+++ script/Akan.js
@@ -0,0 +1,11 @@
+function akanName(){
+var yearOf = document.getElementById("yearOf").value;
+var cc = Number(yearOf.slice(2,));
+var dayOf = Number(document.getElementById("dayOfBirth").value);
+var monthOf = Number(document.getElementById("monthOf").value);
+
+
+
+
+}
+
