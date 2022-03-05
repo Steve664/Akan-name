5a76a9c43b5463e40ae1570425d6a776d348e19c
steve kariuki <steve.kariuki@student.moringaschool.com>
2022-03-06 00:11:06 +0300

    Adjusted html form and added radio button check

--- script/Akan.js
+++ script/Akan.js
@@ -1,10 +1,25 @@
 function akanName() {
-    var yearOf = document.getElementById("yearOf").value;
-    var yy = Number(yearOf.slice(2,));
-    var cc = Number(yearOf.slice(0, 2));
+    let yearOf = document.getElementById("yearOf").value;
+    let yy = Number(yearOf.slice(2,));
+    let cc = Number(yearOf.slice(0, 2));
+    let genders = document.getElementsByName("gender");
+    let dayOf = Number(document.getElementById("dayOfBirth").value);
+    let monthOf = Number(document.getElementById("monthOf").value);
+    let weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-    var dayOf = Number(document.getElementById("dayOfBirth").value);
-    var monthOf = Number(document.getElementById("monthOf").value);
+
+    // function to get gender
+    function getGender() {
+        for (let gender of genders) {
+            if (gender.checked) {
+                return gender.value;
+            }
+        }
+    }
+    let myGenderValue = getGender();
+    console.log(myGenderValue);
 
     function validMonth() {
         if (monthOf < 1 || monthOf > 12) {
@@ -15,22 +30,29 @@ function akanName() {
         }
 
         function validDay() {
-            if (dayOf < 0 || dayOf > 31) { 
-                return false; 
+            if (dayOf < 0 || dayOf > 31) {
+                return false;
             }
-                //to handle for leap year
-            else if (yearOf%4==0 && monthOf == 2 && dayOf > 29) {
+            //to handle for leap year
+            else if (yearOf % 4 == 0 && monthOf == 2 && dayOf > 29) {
                 return false;
             }
             else if (monthOf == 2 && dayOf > 28) {
                 return false;
             }
-        
+            else {
+                return true;
+            }
+
+
+
+
         }
 
 
 
     }
+
 
 
 }
