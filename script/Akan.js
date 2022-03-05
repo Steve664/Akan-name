0837f22c8add49e8e16b4e636e6a073b6a3b42c5
steve kariuki <steve.kariuki@student.moringaschool.com>
2022-03-05 14:11:15 +0300

    Added functions for month and day validation

--- script/Akan.js
+++ script/Akan.js
@@ -1,10 +1,31 @@
-function akanName(){
-var yearOf = document.getElementById("yearOf").value;
-var cc = Number(yearOf.slice(2,));
-var dayOf = Number(document.getElementById("dayOfBirth").value);
-var monthOf = Number(document.getElementById("monthOf").value);
+function akanName() {
+    var yearOf = document.getElementById("yearOf").value;
+    var yy = Number(yearOf.slice(2,));
+    var cc = Number(yearOf.slice(0, 2));
+
+    var dayOf = Number(document.getElementById("dayOfBirth").value);
+    var monthOf = Number(document.getElementById("monthOf").value);
+
+    function validMonth() {
+        if (monthOf < 1 || monthOf > 12) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+        function validDay() {
+            if (dayOf < 0 || dayOf > 31) { 
+                return false; }
+            else if (monthOf == 2 && dayOf > 29) {
+                return false;
+            }
+        
+        }
 
 
+
+    }
 
 
 }
